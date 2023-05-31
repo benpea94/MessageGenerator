aea2987e833d32b9682ee1c243a5f32ec5769177
benpea94 <benpea94@gmail.com>
2023-05-31 16:56:29 +0100

    Added breaks to switch case, updated variables

--- messageGen.js
+++ messageGen.js
@@ -5,25 +5,27 @@ const generateRandNum = num => {
 const randSelections = {
     oneLiners : ["My wife told me to stop impersonating a flamingo. I had to put my foot down.", "I failed maths so many times at school, I can’t even count.", " I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.", " When life gives you melons, you might be dyslexic.", "Pretentious? Moi?"],
     songOutput : ["Everybody Dance", "Thriller", "One Dance", "Back in Black", "Here Comes The Sun"],
-    movieOuput : ["Goal!", "Anchorman", "Gladiator", "Goodfellas", "Mean Girls"],
+    movieOutput : ["Goal!", "Anchorman", "Gladiator", "Goodfellas", "Mean Girls"],
 };
 
 let entertainmentSelection = [];
 
-for (let item in randSelections) {
-    let optionId = generateRandNum(randSelections[item].length)
+for (let arr in randSelections) {
+    let optionId = generateRandNum(randSelections[arr].length)
 
-    switch(item) {
+    switch(arr) {
         case 'oneLiners':
-            entertainmentSelection.push(`Your random joke: ${randSelections[item][optionId]}`)
+            entertainmentSelection.push(`Your random joke: ${randSelections[arr][optionId]}.`)
             break
         case 'songOutput':
-            entertainmentSelection.push(`Your random song: ${randSelections[item][optionId]}`)
+            entertainmentSelection.push(`Your random song: ${randSelections[arr][optionId]}.`)
+            break
         case 'movieOutput':
-            entertainmentSelection.push(`Your random movie: ${randSelections[item][optionId]}`)
+            entertainmentSelection.push(`Your random movie: ${randSelections[arr][optionId]}.`)
+            break
         default:
             entertainmentSelection.push("INVALID INFORMATION")
-
+            break
     }
 };
 
