663e38cdd2658dadc367b3af368fde1bc87f1c21
benpea94 <benpea94@gmail.com>
2023-05-31 16:31:43 +0100

    Added selection object

--- messageGen.js
+++ messageGen.js
@@ -1,3 +1,7 @@
-oneLiners = ["My wife told me to stop impersonating a flamingo. I had to put my foot down.", "I failed maths so many times at school, I can’t even count.", " I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.", " When life gives you melons, you might be dyslexic.", "Pretentious? Moi?"];
+randSelections = {
+    oneLiners : ["My wife told me to stop impersonating a flamingo. I had to put my foot down.", "I failed maths so many times at school, I can’t even count.", " I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.", " When life gives you melons, you might be dyslexic.", "Pretentious? Moi?"],
+    songOutput : ["Everybody Dance", "Thriller", "One Dance", "Back in Black", "Here Comes The Sun"],
+    movieOuput : ["Goal!", "Anchorman", "Gladiator", "Goodfellas", "Mean Girls"],
+}
 
-console.log(oneLiners.length)
+
