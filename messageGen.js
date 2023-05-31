21bd8b899d58417b4f558d11cfd13c88d29138d0
benpea94 <benpea94@gmail.com>
2023-05-31 16:18:57 +0100

    Renamed file

---
+++ messageGen.js
@@ -0,0 +1,3 @@
+oneLiners = ["My wife told me to stop impersonating a flamingo. I had to put my foot down.", "I failed maths so many times at school, I can’t even count.", " I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.", " When life gives you melons, you might be dyslexic.", "Pretentious? Moi?"];
+
+console.log(oneLiners.length)
